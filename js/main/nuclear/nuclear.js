13d119beef3942c949e32a6637ed8f975fa8f09f
c2V2ZW4K <112300765+c2V2ZW4K@users.noreply.github.com>
2023-05-29 01:37:45 +0000

    price option over mass return

--- js/main/nuclear/nuclear.js
+++ js/main/nuclear/nuclear.js
@@ -1,25 +1,40 @@
-    function megatonsToJoules() {
-      var megatons = parseFloat(document.getElementById("megatonsInput").value);
+function megatonsToJoules() {
+  var megatons = parseFloat(document.getElementById("megatonsInput").value);
 
-      if (!isNaN(megatons)) {
-        var joules = megatons * 4.184 * Math.pow(10, 15);
-        var returnMass = document.getElementById("returnMass").checked;
+  if (!isNaN(megatons)) {
+    var joules = megatons * 4.184 * Math.pow(10, 15);
+    var returnMass = document.getElementById("returnMass").checked;
 
-        if (returnMass) {
-          var mass = joules / (8.988 * Math.pow(10, 13));
-          var massInTons = mass / 1000;;
-          document.getElementById("massOutputkg").innerHTML = mass.toFixed(2) + " kg";
-          document.getElementById("massOutputTonnes").innerHTML = massInTons.toFixed(2) + " Tonnes";
-        } else {
-
-        }
-
-        document.getElementById("joulesOutput").innerHTML = joules.toFixed(2) + " joules";
-      } else {
-        document.getElementById("joulesOutput").innerHTML = "";
-
-      }
+    if (returnMass) {
+      var mass = joules / (8.988 * Math.pow(10, 13));
+      var massInTons = mass / 1000;
+      document.getElementById("massOutputkg").innerHTML = mass.toFixed(2) + " kg";
+      document.getElementById("massOutputTonnes").innerHTML = massInTons.toFixed(2) + " Tonnes";
+    } else {
+      document.getElementById("massOutputkg").innerHTML = "";
+      document.getElementById("massOutputTonnes").innerHTML = "";
     }
+
+    document.getElementById("joulesOutput").innerHTML = joules.toFixed(2) + " joules";
+  } else {
+    document.getElementById("joulesOutput").innerHTML = "";
+    document.getElementById("massOutputkg").innerHTML = "";
+    document.getElementById("massOutputTonnes").innerHTML = "";
+  }
+}
+
+function calculateTotalPrice() {
+  var massInKg = parseFloat(document.getElementById("massOutputkg").innerHTML);
+  var pricePerPound = parseFloat(prompt("Please enter the price of uranium per pound:"));
+
+  if (!isNaN(massInKg) && !isNaN(pricePerPound)) {
+    var massInLbs = massInKg * 2.20462;
+    var totalPrice = massInLbs * pricePerPound;
+    document.getElementById("priceOutput").innerHTML = "$" + totalPrice.toFixed(2);
+  } else {
+    alert("Invalid input. Please enter numeric values.");
+  }
+}
 
     function joulesToMegatons() {
       var joules = parseFloat(document.getElementById("joulesInput").value);
