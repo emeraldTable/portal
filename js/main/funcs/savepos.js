5c0bc1298459b983154c894e785e2e78118c5bf6
c2V2ZW4K <112300765+c2V2ZW4K@users.noreply.github.com>
2023-05-29 00:11:39 +0000

    save position javascript

---
+++ js/main/funcs/savepos.js
@@ -0,0 +1,59 @@
+    // Function to save the position of elements with a specific ID into a JSON file
+        function savePositionsToJson() {
+            var positions = {};
+
+            // Define the IDs of the elements whose positions you want to save
+            var elementIds = ["divResize1", "divResize2", "divResize3", "divResize4", "divResize5", "divResize6", "divResize7", "divResize8", "divResize9", "divResize10", "divResize11", "divResize12", "divResize13", "divResize14", "divResize15", "divResize16", "divResize17", "divResize18", "divResize19", "divResize20", "divResize21",];
+
+            elementIds.forEach(function(id) {
+                var element = document.getElementById(id);
+                var position = {
+                    left: element.style.left,
+                    top: element.style.top,
+                    height: element.style.height,
+                    width: element.style.width
+                };
+                positions[id] = position;
+            });
+
+            var json = JSON.stringify(positions);
+            var blob = new Blob([json], { type: 'application/json' });
+
+            // Create a temporary anchor element
+            var downloadLink = document.createElement('a');
+            downloadLink.href = URL.createObjectURL(blob);
+            downloadLink.download = 'positions.json';
+            downloadLink.click();
+        }
+
+        // Function to handle the file upload
+        function handleFileUpload(event) {
+            var file = event.target.files[0];
+            var reader = new FileReader();
+
+            reader.onload = function(e) {
+                var json = e.target.result;
+                importPositionsFromJson(json);
+            };
+
+            reader.readAsText(file);
+        }
+
+        // Function to import position values from JSON and update HTML elements
+        function importPositionsFromJson(json) {
+            var positions = JSON.parse(json);
+
+            for (var id in positions) {
+                if (positions.hasOwnProperty(id)) {
+                    var element = document.getElementById(id);
+                    var position = positions[id];
+                    element.style.left = position.left;
+                    element.style.top = position.top;
+                    element.style.height = position.height;
+                    element.style.width = position.width;
+                }
+            }
+        }
+
+        var savedJson = '{}';
+        importPositionsFromJson(savedJson);
