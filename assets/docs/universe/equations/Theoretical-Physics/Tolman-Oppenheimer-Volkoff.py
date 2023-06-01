e53c5ba31350755309184733aedac33310a1efa9
c2V2ZW4K <112300765+c2V2ZW4K@users.noreply.github.com>
2023-06-01 04:44:40 +0000

    adjusts in the atomic mass / elements: Uranium-238, Plutonium-244, Curium-247

--- assets/docs/universe/equations/Theoretical-Physics/Tolman-Oppenheimer-Volkoff.py
+++ assets/docs/universe/equations/Theoretical-Physics/Tolman-Oppenheimer-Volkoff.py
@@ -1,58 +1,70 @@
 import math
 
-def calculate_properties(radius, mass):
-    G = 6.67430e-11  # Constante gravitacional em m^3/(kg·s^2)
-    c = 299792458  # Velocidade da luz no vácuo em m/s
+# Atomic masses of elements
+atomic_masses = {
+    "plutonium": 244,
+    "uranium": 238,
+    "curium": 247
+}
 
-    volume = (4/3) * math.pi * radius**3
-    density = mass / volume
-    pressure = (G * (density + c**2 * (mass / (4*math.pi*radius**3)))) / (3 * (1 - (2 * G * mass) / (radius * c**2)))
-    dP_dr = - G * (density + pressure) * (mass + (4/3) * math.pi * radius**3 * pressure) / (radius * (radius - (2 * G * mass) / c**2))
+def calculate_properties(r, m, element):
+    G = 6.67430e-11  # Gravitational constant in m^3/(kg·s^2)
+    c = 299792458  # Speed of light in vacuum in m/s
 
-    return density, pressure, dP_dr
+    v = (4/3) * math.pi * r**3
+    ρ = m / v
+    atomic_mass = atomic_masses[element]
+    P = (G * (ρ + c**2 * (m / (4*math.pi*r**3)))) / (3 * (1 - (2 * G * m) / (r * c**2 * atomic_mass)))
+    dP_dr = - G * (ρ + P) * (m + (4/3) * math.pi * r**3 * P) / (r * (r - (2 * G * m) / (c**2 * atomic_mass)))
+
+    return ρ, P, dP_dr
 
 def create_cluster():
-    num_layers = int(input("Digite o número de camadas do cluster: "))
+    num_layers = int(input("Enter the number of layers in the cluster: "))
 
     cluster = []
 
     for i in range(num_layers):
-        print(f"\nCamada {i+1}:")
-        radius_unit = input("Escolha a unidade de medida para o raio (m, cm, mm): ")
-        radius = float(input("Informe o raio: "))
+        print(f"\nLayer {i+1}:")
+        radius_unit = input("Choose the unit of measurement for the radius (1 for m, 2 for cm, 3 for mm): ")
+        radius_option = int(radius_unit)
+        radius = float(input("Enter the radius: "))
 
-        mass_unit = input("Escolha a unidade de massa (t, kg, g, mg): ")
-        mass = float(input("Informe a massa: "))
+        mass_unit = input("Choose the mass unit (1 for t, 2 for kg, 3 for g, 4 for mg): ")
+        mass_option = int(mass_unit)
+        mass = float(input("Enter the mass: "))
 
-        # Conversão das unidades para metros e quilogramas
-        if radius_unit == "cm":
+        element = input("Choose the element (plutonium, uranium, curium): ")
+
+        # Conversion of units to meters and kilograms
+        if radius_option == 2:
             radius /= 100.0
-        elif radius_unit == "mm":
+        elif radius_option == 3:
             radius /= 1000.0
 
-        if mass_unit == "t":
+        if mass_option == 1:
             mass *= 1000.0
-        elif mass_unit == "g":
+        elif mass_option == 3:
             mass /= 1000.0
-        elif mass_unit == "mg":
+        elif mass_option == 4:
             mass /= 1000000.0
 
-        density, pressure, dP_dr = calculate_properties(radius, mass)
-        layer = {"Raio": radius, "Massa": mass, "Densidade": density, "Pressão": pressure, "dP/dr": dP_dr}
+        density, pressure, dP_dr = calculate_properties(radius, mass, element)
+        layer = {"Radius": radius, "Mass": mass, "Density": density, "Pressure": pressure, "dP/dr": dP_dr}
         cluster.append(layer)
 
     return cluster
 
-# Criação do cluster
+# Create the cluster
 cluster = create_cluster()
 
-# Exibição das propriedades de cada camada
-print("\nPropriedades do Cluster:")
+# Display the properties of each layer
+print("\nCluster Properties:")
 
 for i, layer in enumerate(cluster):
-    print(f"\nCamada {i+1}:")
-    print("Raio: {:.2f} metros".format(layer["Raio"]))
-    print("Massa: {:.2f} quilogramas".format(layer["Massa"]))
-    print("Densidade: {:.2f} kg/m^3".format(layer["Densidade"]))
-    print("Pressão: {:.2f} N/m^2".format(layer["Pressão"]))
+    print(f"\nLayer {i+1}:")
+    print("Radius: {:.2f} meters".format(layer["Radius"]))
+    print("Mass: {:.2f} kilograms".format(layer["Mass"]))
+    print("Density: {:.2f} kg/m^3".format(layer["Density"]))
+    print("Pressure: {:.2f} N/m^2".format(layer["Pressure"]))
     print("dP/dr: {:.2f} N/(m^2/m)".format(layer["dP/dr"]))
