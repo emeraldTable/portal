d4471b5085a1a5acd61f274e682ebc02beb11f3f
c2V2ZW4K <112300765+c2V2ZW4K@users.noreply.github.com>
2023-06-07 22:47:29 +0000

    v0.0.3 with biologic rules inserted

--- assets/addons/python/proteins/sequencer/bin/sequencer.py
+++ assets/addons/python/proteins/sequencer/bin/sequencer.py
@@ -1,13 +1,32 @@
 import random
 
 def gerador_sequencia():
-    amino_acidos = ["Met", "Arg", "Thr", "Ser", "Glu", "Lys", "Gln", "Gly", "Ala", "Val", "Leu", "Cys", "Asp", "Phe", "Ile", "Asn"]
+    amino_acidos = {
+        "Met": "AUG",
+        "Arg": ["CGU", "CGC", "CGA", "CGG", "AGA", "AGG"],
+        "Thr": ["ACU", "ACC", "ACA", "ACG"],
+        "Ser": ["UCU", "UCC", "UCA", "UCG", "AGU", "AGC"],
+        "Glu": ["GAA", "GAG"],
+        "Lys": ["AAA", "AAG"],
+        "Gln": ["CAA", "CAG"],
+        "Gly": ["GGU", "GGC", "GGA", "GGG"],
+        "Ala": ["GCU", "GCC", "GCA", "GCG"],
+        "Val": ["GUU", "GUC", "GUA", "GUG"],
+        "Leu": ["UUA", "UUG", "CUU", "CUC", "CUA", "CUG"],
+        "Cys": ["UGU", "UGC"],
+        "Asp": ["GAU", "GAC"],
+        "Phe": ["UUU", "UUC"],
+        "Ile": ["AUU", "AUC", "AUA"],
+        "Asn": ["AAU", "AAC"]
+    }
 
     sequencia = ""
-    comprimento = random.randint(100, 200)  # Defina o comprimento desejado da sequência
+    comprimento = 250
 
     for _ in range(comprimento):
-        amino_acido = random.choice(amino_acidos)
+        amino_acido = random.choice(list(amino_acidos.keys()))
+        codons = amino_acidos[amino_acido]
+        codon = random.choice(codons)
         sequencia += amino_acido + "-"
 
     sequencia = sequencia[:-1]  # Remove o último hífen
