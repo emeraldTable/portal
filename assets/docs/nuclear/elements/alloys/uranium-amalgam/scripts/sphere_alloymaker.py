c79e235279171b3869f54e9a7302ce9ee883c905
c2V2ZW4K <112300765+c2V2ZW4K@users.noreply.github.com>
2023-06-11 19:45:48 +0000

    Uranium Amalgam > sphere_alloymaker.py > Density equation

--- assets/docs/nuclear/elements/alloys/uranium-amalgam/scripts/sphere_alloymaker.py
+++ assets/docs/nuclear/elements/alloys/uranium-amalgam/scripts/sphere_alloymaker.py
@@ -17,15 +17,22 @@ def calculate_amalgam_amount(thickness_cm, radius_m):
     volume_uranium = 0.8 * volume_solid_sphere
     volume_mercury = 0.2 * volume_solid_sphere
 
-    return volume_uranium, volume_mercury
+    # Calculate the density of the uranium amalgam
+    mass_uranium = volume_uranium * 19050  # Assuming uranium density of 19,050 kg/m³
+    mass_mercury = volume_mercury * 13534  # Assuming mercury density of 13,534 kg/m³
+    total_mass = mass_uranium + mass_mercury
+    density = total_mass / (volume_uranium + volume_mercury)
+
+    return volume_uranium, volume_mercury, density
 
 # User input
 thickness = float(input("Enter the thickness of the sphere in centimeters: "))
 radius = float(input("Enter the radius of the sphere in meters: "))
 
-# Calculate the amount of uranium and mercury
-uranium, mercury = calculate_amalgam_amount(thickness, radius)
+# Calculate the amount of uranium, mercury, and density
+uranium, mercury, density = calculate_amalgam_amount(thickness, radius)
 
 # Print the results
 print(f"Amount of uranium: {uranium} cubic meters")
 print(f"Amount of mercury: {mercury} cubic meters")
+print(f"Density of the uranium amalgam: {density} kg/m³")
